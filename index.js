0890578904c06bc176e01c38765265cc068257da
sararita28 <67070886+sararita28@users.noreply.github.com>
2021-12-21 10:07:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,27 @@
 const express = require('express')
-const path = require('path')
 
+    //express app
 const app = express()
+
 const port = process.env.PORT || 8080
 
+const server = app.listen(port)
+    //index page
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'))
+    res.sendFile('./index.html', {root: __dirname})
 })
-
 app.get('/index.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'))
+    res.sendFile('./index.html', {root: __dirname})
 })
-
+    //about page
 app.get('/about.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'about.html'))
+    res.sendFile('./about.html', {root: __dirname})
 })
-
+    //contact page
 app.get('/contact-me.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'contact-me.html'))
+    res.sendFile('./contact-me.html', {root: __dirname})
 })
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '404.html'))
-})
-
-app.listen((err) => {
-    if(err) {
-        console.log(err)
-    } else {
-        console.log('Listening on port:' + port)
-    }
+    //404 page
+app.use((req, res) => {
+    res.status(404).sendFile('./404.html', { root: __dirname})
 })
