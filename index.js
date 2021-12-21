7a89b78346b8d86b8fc2cb6417e8888cfcc9217d
sararita28 <67070886+sararita28@users.noreply.github.com>
2021-12-21 06:25:05 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+    next()
+})
+
+app.get('/index.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+    next()
+})
+
+app.get('/about.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'about.html'))
+})
+
+app.get('/contact-me.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'contact-me.html'))
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '404.html'))
+})
+
+app.listen((err) => {
+    if(err) {
+        console.log(err)
+    } else {
+        console.log('Listening on port:' + port)
+    }
+})
